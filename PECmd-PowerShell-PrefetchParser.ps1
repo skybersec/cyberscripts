2dad000aa4f185d77d888f111d94f893f37910e0
digitalnodedox <34582724+digitalnodedox@users.noreply.github.com>
2020-12-05 21:23:11 -0800

    Update and rename prefetchParser.ps1 to PECmd-PowerShell-PrefetchParser.ps1

---
+++ PECmd-PowerShell-PrefetchParser.ps1
@@ -0,0 +1,5 @@
+#Pre-req Download PECmd :)
+$files = Get-ChildItem "C:\Windows\Prefetch\" -Filter *.EXE*.pf
+foreach ($f in $files){
+    .\PECmd.exe -f C:\Windows\Prefetch\$f >> C:\outputfolder\$f.txt
+}
--- prefetchParser.ps1
+++
@@ -1,4 +0,0 @@
-﻿$files = Get-ChildItem "C:\Windows\Prefetch\" -Filter *.EXE*.pf
-foreach ($f in $files){
-    .\PECmd.exe -f C:\Windows\Prefetch\$f >> C:\outputfolder\$f.txt
-}
